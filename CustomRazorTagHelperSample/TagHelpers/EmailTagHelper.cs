4755ad815d7fc94ec8bee7b9fe5cead44ab70c7f
Rajeesh VP <rajeeshvp@gmail.com>
2020-09-14 13:58:51 +0530

    Tag helper "email" is created

---
+++ CustomRazorTagHelperSample/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomRazorTagHelperSample.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var address = MailTo + "@gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+Asp.Net Core razor custom Tag Helper code sample as described at https://docs.microsoft.com/en-us/aspnet/core/mvc/views/tag-helpers/authoring?view=aspnetcore-3.1
+© Microsoft 2020
+
+Sample verified by rajeeshvp@gmail.com
+Files Added/Changed
+	1. MyPage.cshtml
+	2. _ViewImports.cshtml
+	3. EmailTagHelper.cs
+Run https://localhost:port/mypage to see the implementation
+
+What it does:
+	<email mail-to="rajeeshvp"></email> 
+		will be converted into
+	<a href="mailto:rajeeshvp@gmail.com">rajeeshvp@gmail.com</a>
